b9440a4378538f680a2850b75b0ecd22060372fb
Akerke <71389869+aidynkyzy@users.noreply.github.com>
2020-09-25 23:12:53 +0600

    Add files via upload

--- 8.py
+++ 8.py
@@ -1,5 +1,5 @@
 widgets  = int(input("Enter the number of widgets: "))
 gizmos  = int(input("Enter the number of gizmos: "))
 
-total = (widgets * 75) + (gizmos * 112);
-print("The total weight of the order is: {} grams".format(total));
+total = (widgets * 75) + (gizmos * 112)
+print("The total weight of the order is: {} grams".format(total))
