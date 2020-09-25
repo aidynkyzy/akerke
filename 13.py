0d3f390a8c26498b4971867d0ca537a10c6bba3e
Akerke <71389869+aidynkyzy@users.noreply.github.com>
2020-09-25 18:57:53 +0600

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,8 +1,8 @@
-c=int(input('Please enter an amount between 0-99:'))
-print(c//25, "quarters")
-c = c%25
-print(c//10, "dimes")
-c = c%10
-print(c//5, "nickles")
-c = c%5
-print(c//1, "pennies")
+a=int(input('Enter an amount between 0-99:'))
+print(a//25, "quarters")
+a = a%25
+print(a//10, "dimes")
+a = a%10
+print(a//5, "nickles")
+a = a%5
+print(a//1, "pennies")
