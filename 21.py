66d02f6de842b7c796eac247dc2d95778724d748
Akerke <71389869+aidynkyzy@users.noreply.github.com>
2020-09-25 19:36:02 +0600

    Update 21.py

--- 21.py
+++ 21.py
@@ -2,6 +2,6 @@ print("The length of the base of the triangle:")
 base = int(input())
 print("The height of the triangle:")
 height = int(input())
-area = 0.5  * height * base
+area =  height * base* 0,5
 ans = "The area of the triangle is {} with the base length {} and height {}"
 print(ans.format(area, base, height))
