f4f1ad83a13bfb1692508624293a8488c4c0db0d
Akerke <71389869+aidynkyzy@users.noreply.github.com>
2020-09-25 19:38:16 +0600

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,5 @@
-length = float(input('Enter the field length in feet: '))
-width = float(input('Enter the field length in feet: '))
+l = float(input('Enter the field length in feet: '))
+w = float(input('Enter the field length in feet: '))
 feet_in_an_acre = 43560
-area = (length * width) / feet_in_an_acre
+area = (l * w) / feet_in_an_acre
 print('Area of the field {} acre'.format(area))
