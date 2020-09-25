1842d52af0ad606b73cfacb47ae958869f3db9e8
Akerke <71389869+aidynkyzy@users.noreply.github.com>
2020-09-25 19:41:57 +0600

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,14 +1,14 @@
-cost_of_feed = int(input("Enter the cost of food: "))
+cost_of_food = int(input("Enter the cost of food: "))
 
 tax = 1.05
 
-cost_of_feed_tax = tax * cost_of_feed
+cost_of_food_tax = tax * cost_of_food
 
 tip = 1.15
 
-cost_of_feed_tip = tip * cost_of_feed_tax
+cost_of_food_tip = tip * cost_of_food_tax
 
 
-print("With tax: {}".format(cost_of_feed_tax))
-print("With tip: {}".format(cost_of_feed_tip))
+print("With tax: {}".format(cost_of_food_tax))
+print("With tip: {}".format(cost_of_food_tip))
                    
